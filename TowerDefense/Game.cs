f1d6c1af0f47373698b418ba41668b3d2a5439ea
David <dpark543107@gmail.com>
2016-05-14 11:08:32 -0500

    Added point.cs and added constructors to the point and map class.

--- TowerDefense/Game.cs
+++ TowerDefense/Game.cs
@@ -4,9 +4,13 @@ namespace TowerDefense
 {
     class Game
     {
-        public void Main()
+        public static void Main()
         {
-            Tower tower = new Tower();
+            Map map = new Map(8, 5);
+
+            int area = map.Width * map.Height;
+            Console.WriteLine(area);
+            Console.ReadLine();
         }
     }
 }
