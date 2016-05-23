fdcae033aaf31f4c552121e85afa2416d120ebbc
David <dpark543107@gmail.com>
2016-05-22 23:06:06 -0500

    Added invaders, towers, random hit or miss, and game win or loss.

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -4,5 +4,50 @@ namespace TowerDefense
 {
     class Tower
     {
+        private const int _range = 1;
+        private const int _power = 1;
+        private const double _accuracy = .75;
+
+        private static readonly Random _random = new System.Random();
+
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            foreach(Invader invader in invaders)
+            {
+                // Do stuff with invader
+                if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                {
+                    if (IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Shot at and hit an invader!");
+                        Console.ReadLine();
+
+                        if (invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Neutralized an invader!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot at and missed an invader!");
+                        Console.ReadLine();
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
